d1ff8c05a96fee875d3093bdab9fe934b59e7be7
Sebastian Gontkovič <sebascng@MacBook-Pro-uzivatela-Sebastian.local>
2024-03-30 15:29:08 +0100

    version-change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="datewise",
-    version="0.1.4",
+    version="0.1.5",
     description="Thesis Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
