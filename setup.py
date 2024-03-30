739ddcde1332e45fc28c558106680b2698ae8c48
Sebastian Gontkovič <sebascng@MacBook-Pro-uzivatela-Sebastian.local>
2024-03-30 12:00:41 +0100

    API-token-final-final-v1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="datewise",
-    version="0.1.2",
+    version="0.1.3",
     description="Thesis Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
