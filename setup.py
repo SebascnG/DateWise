f89e103118ada24d550ad8c19bc3c39f46e940ac
Sebastian Gontkovič <sebascng@MacBook-Pro-uzivatela-Sebastian.local>
2024-05-02 19:09:47 +0200

    Code-changes

--- setup.py
+++ setup.py
@@ -2,17 +2,14 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-# The directory containing this file
 HERE = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# This call to setup() does all the work
 setup(
     name="datewise",
-    version="0.2.2",
+    version="1.0.0",
     description="Thesis Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
