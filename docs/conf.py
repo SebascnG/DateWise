7d8e5ed2c35b76cbcba26d6f38e9df255ce969e0
Sebastian Gontkovič <sebascng@MacBook-Pro-uzivatela-Sebastian.local>
2024-05-02 19:21:18 +0200

    code-changes-v2

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,12 @@
 import os
 import sys
+
+# Setting up the root folder
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'datewise'
-copyright = '2024, Sebastian Gontkovic'
 author = 'Sebastian Gontkovic'
+copyright = 'Sebastian Gontkovic, 2024, Bratislava'
 
 extensions = [
     'recommonmark',
@@ -13,7 +15,7 @@ extensions = [
 ]
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
+html_static_path = ['_static']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup, find_packages
-from codecs import open
 from os import path
+from codecs import open
+from setuptools import setup, find_packages
 
-HERE = path.abspath(path.dirname(__file__))
+current_path = path.abspath(path.dirname(__file__))
 
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(current_path, 'README.md'), encoding='utf-8') as f:
+    desc = f.read()
 
 setup(
     name="datewise",
     version="1.0.0",
     description="Thesis Library",
-    long_description=long_description,
+    long_description=desc,
     long_description_content_type="text/markdown",
     url="https://datewise.readthedocs.io/",
     author="Sebastian Gontkovic",
